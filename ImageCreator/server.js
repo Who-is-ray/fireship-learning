867dc84c6d4ef8f7b72dc4bdfef744fc58afd354
ruiyuansu <ruiyuansu@microsoft.com>
2023-08-15 13:00:03 -0700

    Setup the node server

---
+++ ImageCreator/server.js
@@ -0,0 +1,37 @@
+import * as dotenv from "dotenv"
+
+import { Configuration, OpenAIApi } from 'openai';
+
+import cors from 'cors';
+import express from 'express';
+
+dotenv.config(); // to access .env file
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI, // OPENAI key in .env file
+});
+
+const openai = new OpenAIApi(configuration); // the openAI API
+
+const app = express();
+app.use(cors()); // security mechanism of cross version resource sharing
+app.use(express.json()); // only handle json format incoming data
+
+app.post('/dream', async (req, res) => {
+    // retrive users image requirement prompt
+    const prompt = req.body.prompt;
+
+    // get openai response
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+    });
+
+    // sending response
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+});
+
+// start to run the server on port 8080
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
